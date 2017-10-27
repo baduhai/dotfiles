53d18b0a9bfd88f0962bbd04e1e1eeb273c1ad3e
baduhai <williamhai@hotmail.com>
2017-10-27 16:21:06 +0200

    Work in progress

---
+++ InstallScript.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+# Script made to be run after a fresh install, may not work depending on the distro. (If you are not me and have come accross this script, I can't stop you from running it, but be ware that this was written for personal use)
