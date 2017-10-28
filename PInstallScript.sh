442d47f294f24c65d091db234ffd5ce62871b10f
baduhai <williamhai@hotmail.com>
2017-10-28 21:53:17 +0200

    Created PIsntallScript

---
+++ PInstallScript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#===============================================================================
+#
+#                                    NOTE!
+#
+# If you are not me and have come accross this script, I can't stop you from
+# running it, but be ware that this was written for personal use, if any harm
+# comes from you running this script, it's not my fault.
+#
+#===============================================================================
+
+#===============================
+# Welcome prompt & safety check
+#===============================
