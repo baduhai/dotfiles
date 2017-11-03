a61f239180e6a847bee1fc08501769ca6df2e138
baduhai <williamhai@hotmail.com>
2017-11-03 16:08:18 +0100

    Solus section PinstallScript done. Needs testing

--- PInstallScript.sh
+++ PInstallScript.sh
@@ -20,34 +20,34 @@ if [[ $danswer = 1 ]] ; then
   sudo pacman -Syu
   echo -n -e "Please choose your environment: \n [Wayland/Xorg]Gnome(1) \n [Xorg]OpenBox(2) \n [Wayland]Sway(3) \n [Wayland]Way Cooler(4) \n [Wayland/Xorg]KDE(5) \n [Xorg]Budgie(6) \n==>" read eanswer
   if [[ $eanswer = 1 ]] ; then
-    sudo pacman -S gnome gnome-extras gnome-tweak-tool jshon
+    sudo pacman -S --noconfirm gnome gnome-extras gnome-tweak-tool jshon
     curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
-    bash ./apacman -S apacman
+    bash ./apacman -S --noconfirm apacman
     rm -rf apacman
   if [[ $eanswer = 2 ]] ; then
-    sudo pacman -S openbox tint2 menumaker lxappearance feh jshon
+    sudo pacman -S --noconfirm openbox tint2 menumaker lxappearance feh jshon
     curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
-    bash ./apacman -S apacman
+    bash ./apacman -S --noconfirm apacman
     rm -rf apacman
   if [[ $eanswer = 3 ]] ; then
-    sudo pacman -S sway jshon
+    sudo pacman -S --noconfirm sway jshon
     curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
-    bash ./apacman -S apacman
+    bash ./apacman -S --noconfirm apacman
     rm -rf apacman
   if [[ $eanswer = 4 ]] ; then
     curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
-    bash ./apacman -S apacman
+    bash ./apacman -S --noconfirm apacman
     rm -rf apacman
-    apacman -S way-cooler way-cooler-bg
+    apacman -S --noconfirm way-cooler way-cooler-bg
   if [[ $eanswer = 5 ]] ; then
-    sudo pacman -S plasma-desktop latte-dock plasma-wayland-session
+    sudo pacman -S --noconfirm plasma-desktop latte-dock plasma-wayland-session
     curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
-    bash ./apacman -S apacman
+    bash ./apacman -S --noconfirm apacman
     rm -rf apacman
   if [[ $eanswer = 6 ]] ; then
-    sudo pacman -S budgie-desktop
+    sudo pacman -S --noconfirm budgie-desktop
     curl -O "https://raw.githubusercontent.com/oshazard/apacman/master/apacman"
-    bash ./apacman -S apacman
+    bash ./apacman -S --noconfirm apacman
     rm -rf apacman
   fi
 if [[ $danswer = 2 ]] ; then
@@ -70,19 +70,24 @@ if [[ $danswer = 2 ]] ; then
 if [[ $danswer = 3 ]] ; then
   repos upate command for Solus
   repos upgrade command for Solus
-  echo -n -e "Please choose your environment: \n [Wayland/Xorg]Gnome(1) \n [Xorg]OpenBox(2) \n [Wayland]Sway(3) \n [Wayland]Way Cooler(4) \n [Wayland/Xorg]KDE(5) \n [Xorg]Budgie(6) \n==>" read eanswer3
+  echo -n -e "Please choose your environment: \n [Wayland/Xorg]Gnome(1) \n [Xorg]OpenBox(2) \n [Wayland]Sway(3) \n [Wayland]Way Cooler(4) \n [Xorg]KDE(5) \n [Xorg]Budgie(6) \n==>" read eanswer3
   if [[ $eanswer3 = 1 ]] ; then
-    Gnome
+    sudo eopkg install -y gnome-shell
   if [[ $eanswer3 = 2 ]] ; then
-    OpenBox
+    sudo eopkg install -y openbox
   if [[ $eanswer3 = 3 ]] ; then
-    Sway
+    sudo eopkg install -y sway
   if [[ $eanswer3 = 4 ]] ; then
-    Way Cooler
+    curl https://way-cooler.github.io/way-cooler-release-i3-default.sh -sSLf | bash -s  way-cooler-bg  wc-grab  wc-lock
   if [[ $eanswer3 = 5 ]] ; then
-    KDE
+    sudo eopkg install -y plasma-desktop
   if [[ $eanswer3 = 6 ]] ; then
-    Budgie
+    echo -n -e "Are you sure Budgie is not already installed? (y/N) \n==>" read banswer
+    if [[ $banswer = "y" ]]; then
+      sudo eopkg install -y budgie-desktop
+    else
+      echo "Get out of here!" && sleep 2 && exit
+    fi
   fi
 if [[ $danswer = 4 ]] ; then
   repos upate command for Ubuntu
@@ -104,7 +109,7 @@ if [[ $danswer = 4 ]] ; then
 if [[ $danswer = 5 ]] ; then
   echo -n -e "Please understand that I don't know which package manager to use, I can only build Way Cooler from source, would you like to continue? (y/N) \n==>" read eanswer5
   if [[ $eanswer5 = "y" ]] ; then
-    build
+    curl https://way-cooler.github.io/way-cooler-release-i3-default.sh -sSLf | bash -s  way-cooler-bg  wc-grab  wc-lock
   else
     echo -n "Sorry not much I can do, bye." && sleep 2 && exit
   fi
