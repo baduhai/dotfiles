c92960850e7a063b65449017588be73cb8deba34
baduhai <williamhai@hotmail.com>
2018-04-06 17:14:38 +0200

    minor changes

--- .scripts/RandomTile.sh
+++ .scripts/RandomTile.sh
@@ -5,11 +5,11 @@ dark="/home/william/Pictures/tiles/dark/"
 light="/home/william/Pictures/tiles/light/"
 
 while true; do
-		if [[ $hour -ge 18 ]]; then
+		if [[ $hour -ge 17 ]]; then
 				cd $dark
 		else
 				cd $light
 		fi
 		feh --bg-tile "$(ls | shuf -n1)"
-		sleep 3600
+		sleep 1800
 done
