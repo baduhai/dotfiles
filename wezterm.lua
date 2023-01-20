3111b8a59366d125e0ba6d47413e4a3c72351139
baduhai <baduhai@pm.me>
2023-01-20 17:52:15 -0300

    wezterm

--- wezterm.lua
+++ wezterm.lua
@@ -1,6 +1,13 @@
 local wezterm = require 'wezterm';
 local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
+local TAB_BAR_BG = "#1d2021"
+local ACTIVE_TAB_BG = "#fe8019"
+local ACTIVE_TAB_FG = "Black"
+local NORMAL_TAB_BG = "#7c6f64"
+local NORMAL_TAB_FG = "Black"
+local HOVER_TAB_BG = "#fabd2f"
+local HOVER_TAB_FG = "Black"
 wezterm.add_to_config_reload_watch_list(wezterm.config_dir)
 return {
   font_size = 11,
@@ -13,26 +20,31 @@ return {
   initial_cols = 108,
   initial_rows = 32,
   tab_bar_at_bottom = true,
-  tab_bar_style = {
-    active_tab_left = wezterm.format({
-      { Background = { Color = "#0b0022" } },
-      { Foreground = { Color = "#2b2042" } },
-      { Text = SOLID_LEFT_ARROW },
-    }),
-    active_tab_right = wezterm.format({
-      { Background = { Color = "#0b0022" } },
-      { Foreground = { Color = "#2b2042" } },
-      { Text = SOLID_RIGHT_ARROW },
-    }),
-    inactive_tab_left = wezterm.format({
-      { Background = { Color = "#0b0022" } },
-      { Foreground = { Color = "#1b1032" } },
-      { Text = SOLID_LEFT_ARROW },
-    }),
-    inactive_tab_right = wezterm.format({
-      { Background = { Color = "#0b0022" } },
-      { Foreground = { Color = "#1b1032" } },
-      { Text = SOLID_RIGHT_ARROW },
-    }),
-  },
+  use_fancy_tab_bar = false,
+	tab_bar_style = {
+		new_tab = wezterm.format({
+			{ Background = { Color = HOVER_TAB_BG } },
+			{ Foreground = { Color = TAB_BAR_BG } },
+			{ Text = SOLID_RIGHT_ARROW },
+			{ Background = { Color = HOVER_TAB_BG } },
+			{ Foreground = { Color = HOVER_TAB_FG } },
+			{ Text = " + " },
+			{ Background = { Color = TAB_BAR_BG } },
+			{ Foreground = { Color = HOVER_TAB_BG } },
+			{ Text = SOLID_RIGHT_ARROW },
+		}),
+		new_tab_hover = wezterm.format({
+			{ Attribute = { Italic = false } },
+			{ Attribute = { Intensity = "Bold" } },
+			{ Background = { Color = NORMAL_TAB_BG } },
+			{ Foreground = { Color = TAB_BAR_BG } },
+			{ Text = SOLID_RIGHT_ARROW },
+			{ Background = { Color = NORMAL_TAB_BG } },
+			{ Foreground = { Color = NORMAL_TAB_FG } },
+			{ Text = " + " },
+			{ Background = { Color = TAB_BAR_BG } },
+			{ Foreground = { Color = NORMAL_TAB_BG } },
+			{ Text = SOLID_RIGHT_ARROW },
+		}),
+	},
 }
