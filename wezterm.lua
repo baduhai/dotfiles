42828ebdb69a4a616a8d264a818943ef14880973
baduhai <baduhai@pm.me>
2023-01-26 20:35:42 -0300

    Wezterm catculations

--- wezterm.lua
+++ wezterm.lua
@@ -1,8 +1,22 @@
 local wezterm = require 'wezterm';
-wezterm.add_to_config_reload_watch_list(wezterm.config_dir)
+
+
+
+local function compute_scheme()
+  local time = tonumber(os.date("%H"))
+  local just_dark = false
+  if time >= 18 or time < 7 or just_dark then
+    return "Catppuccin Mocha"
+  else
+    return "Catppuccin Latte"
+  end
+end
+
+
+
 return {
   font_size = 11,
-  color_scheme = 'Catppuccin Mocha',
+  color_scheme = compute_scheme(),
   hide_tab_bar_if_only_one_tab = true,
   font = wezterm.font_with_fallback ({
     "Hack Nerd Font",
