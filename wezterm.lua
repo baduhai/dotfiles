d867e6746e8ca990f8f39157adc6a5b78e81a351
baduhai <baduhai@pm.me>
2023-01-26 19:52:17 -0300

    Catppuccin colors

--- wezterm.lua
+++ wezterm.lua
@@ -1,17 +1,8 @@
 local wezterm = require 'wezterm';
-local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-local SOLID_RIGHT_ARROW = utf8.char(0xe0b0)
-local TAB_BAR_BG = "#1d2021"
-local ACTIVE_TAB_BG = "#fe8019"
-local ACTIVE_TAB_FG = "Black"
-local NORMAL_TAB_BG = "#7c6f64"
-local NORMAL_TAB_FG = "Black"
-local HOVER_TAB_BG = "#fabd2f"
-local HOVER_TAB_FG = "Black"
 wezterm.add_to_config_reload_watch_list(wezterm.config_dir)
 return {
   font_size = 11,
-  color_scheme = 'Kasugano (terminal.sexy)',
+  color_scheme = 'Catppuccin Mocha',
   hide_tab_bar_if_only_one_tab = true,
   font = wezterm.font_with_fallback ({
     "Hack Nerd Font",
