1a47edaab59591f692bb979912a8da88d6b50633
William <baduhai@proton.me>
2023-08-17 12:18:42 -0300

    tweaked wexterm a bit more

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,13 +4,13 @@ function scheme_for_appearance(appearance)
   if appearance:find "Dark" then
     return "Catppuccin Mocha"
   else
-    return "Catppuccin Frappe"
+    return "Catppuccin Macchiato"
   end
 end
 
 return {
   font_size = 11,
-  color_scheme = "Catpuccin Mocha",
+  color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
   hide_tab_bar_if_only_one_tab = true,
   font = wezterm.font_with_fallback ({
     "Hack Nerd Font",
